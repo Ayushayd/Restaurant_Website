78b5b0e58d7f8215815acefd613831b4dc2ebe0f
Ayush <127881117+Ayushayd@users.noreply.github.com>
2024-04-19 18:48:46 +0530

    ok

--- frontend/src/Backend_URL.js
+++ frontend/src/Backend_URL.js
@@ -1,2 +1,2 @@
 // export const BASE_URL = "http://localhost:3000/api/v1";
-export const BASE_URL = "https://restaurant-website-0n9t.onrender.com/";
+export const BASE_URL = "https://restaurant-website-0n9t.onrender.com";
